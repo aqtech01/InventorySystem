28edef551a3a89d05eae5568392595705c3253e7
aqtech <abdulqadeer0120@gmail.com>
2024-10-10 01:38:20 +0100

    project create

---
+++ src/app/dashboard/page.js
@@ -0,0 +1,14 @@
+import React from "react";
+import MainPage from "./../components/Main";
+export const metadata = {
+  title: "AQStore.Dashboard",
+};
+export default function DashboardPage() {
+  return (
+    <>
+      <MainPage>
+        <h1 class="">This is Dashboard</h1>
+      </MainPage>
+    </>
+  );
+}
