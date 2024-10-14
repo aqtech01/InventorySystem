3f5ca12299159c849a767b3743bb51c4168ebacf
MashalTech-rgb <mashalpugc@gmail.com>
2024-10-13 23:31:52 -0700

    comiy

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,9 @@
 import Image from "next/image";
 import MainPage from "./components/Main";
 import Header from "./components/Header";
-import footer from "./components/Footer";
-import Footer from "./components/Footer";
 import SideBar from "./components/sideBar";
 export default function Home() {
-  return (
+  return ( 
     <>
       <div className="">
         <div class="mb-0"></div>
@@ -13,8 +11,6 @@ export default function Home() {
           <SideBar />
         </div>
       </div>
-
-      {/* <Footer/> */}
     </>
   );
 }
